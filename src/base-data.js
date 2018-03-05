ffdcb459ab7f54fe99a385b134eb9777b149ed0a
Matiss Janis Aboltins <matiss@mja.lv>
2018-03-05 21:25:53 +0200

    Output various statistics

--- src/base-data.js
+++ src/base-data.js
@@ -1,27 +1,52 @@
 'use strict';
 
-const mysql = require('mysql2/promise');
-const moment = require('moment');
+const mysql   = require('mysql2/promise');
+const moment  = require('moment');
+const numbers = require('numbers');
 
 export const run = async (event, context, callback) => {
   const connection = await mysql.createConnection({
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE,
+    host:           process.env.DB_HOST,
+    user:           process.env.DB_USERNAME,
+    password:       process.env.DB_PASSWORD,
+    database:       process.env.DB_DATABASE,
     connectTimeout: 1000,
   });
 
   const start = moment.utc().startOf('day').toISOString();
   const end = moment.utc().endOf('day').toISOString();
 
-  const [data, fields] = await connection.execute(
-    'SELECT id FROM properties WHERE created_at BETWEEN ? AND ?',
-    [start, end],
-  );
+  const [data, fields] = await connection.query({
+    sql: `
+      SELECT price
+      FROM properties
+      WHERE created_at BETWEEN ? AND ?
+      AND price > 0
+      AND type = ?
+    `,
+
+    values: [start, end, 'sell'],
+
+    typeCast(field, next) {
+      if (field.type === 'NEWDECIMAL') {
+        return parseFloat(field.string());
+      }
+
+      return next();
+    },
+  });
+
+  const prices = data.map(({ price }) => price);
 
   const response = {
-    matches: data.length,
+    count:        prices.length,
+    max:          parseInt(numbers.basic.max(prices), 10),
+    min:          parseInt(numbers.basic.min(prices), 10),
+    min:          parseInt(numbers.basic.min(prices), 10),
+    mean:         parseInt(numbers.statistic.mean(prices), 10),
+    median:       parseInt(numbers.statistic.median(prices), 10),
+    mode:         parseInt(numbers.statistic.mode(prices), 10),
+    standardDev:  parseInt(numbers.statistic.standardDev(prices), 10),
   };
 
   connection.end();
